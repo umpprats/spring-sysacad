91713ecf4f3a16771659252bf6b8b31ca1ce36ba
Pablo Prats <pablo.prats@um.edu.ar>
2025-03-30 13:46:32 -0300

    init

---
+++ src/main/java/ar/edu/utn/frsr/sysacad/sysacad/SysacadApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frsr.sysacad.sysacad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysacadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysacadApplication.class, args);
+	}
+
+}
